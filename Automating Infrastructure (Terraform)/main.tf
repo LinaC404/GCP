42e9be55fa800399dacf4de715dfc3025573c4f9
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-21 16:35:19 +0900

    Update main.tf

--- Automating Infrastructure (Terraform)/main.tf
+++ Automating Infrastructure (Terraform)/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
 provider "google" {
   project = var.project_id
   region = var.region
