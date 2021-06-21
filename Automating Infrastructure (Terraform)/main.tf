0a47840feb2d3a0750ed73225a52754aa328da13
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-21 11:49:53 +0900

    Update main.tf

--- Automating Infrastructure (Terraform)/main.tf
+++ Automating Infrastructure (Terraform)/main.tf
@@ -1,1 +1,13 @@
+provider "google" {
+  project = var.project_id
+  region = var.region
+  zone = var.zone
+}
 
+module "instances" {
+  source     = "./modules/instances"
+}
+
+module "storage" {
+  source     = "./modules/storage"
+}
