872a7e44fd85fbf7fb51e5abc9dd528e545aeffb
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-21 11:58:02 +0900

    Update instances.tf

--- Automating Infrastructure (Terraform)/modules/instances/instances.tf
+++ Automating Infrastructure (Terraform)/modules/instances/instances.tf
@@ -1,1 +1,49 @@
 
+resource "google_compute_instance" "tf-instance-1" {
+  name         = "tf-instance-1"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20210609"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+  
+  metadata_startup_script = <<-EOT
+          #!/bin/bash
+      EOT
+  
+  allow_stopping_for_update = true
+}
+
+
+resource "google_compute_instance" "tf-instance-2" {
+  name         = "tf-instance-2"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20210609"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+  
+  metadata_startup_script = <<-EOT
+          #!/bin/bash
+      EOT
+  
+  allow_stopping_for_update = true
+}
