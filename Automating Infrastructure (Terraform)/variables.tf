ba1363cf79d5b1d2dde8ce743215653a30ec591b
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-21 11:52:58 +0900

    Update variables.tf

--- Automating Infrastructure (Terraform)/variables.tf
+++ Automating Infrastructure (Terraform)/variables.tf
@@ -1,1 +1,13 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "<FILL IN YOUR PROJECT ID HERE>"
+}
 
+variable "region" {
+ default = "us-central1"
+}
+
+variable "zone" {
+ default = "us-central1-a"
+}
