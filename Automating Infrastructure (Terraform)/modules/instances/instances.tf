accf8375ae300235b2a03703dbe023aca881e44b
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-21 17:39:10 +0900

    Update instances.tf

--- Automating Infrastructure (Terraform)/modules/instances/instances.tf
+++ Automating Infrastructure (Terraform)/modules/instances/instances.tf
@@ -1,6 +1,7 @@
 resource "google_compute_instance" "tf-instance-1" {
   name         = "tf-instance-1"
-  machine_type = "n1-standard-1"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
   allow_stopping_for_update = true
 
   boot_disk {
@@ -17,7 +18,8 @@ resource "google_compute_instance" "tf-instance-1" {
 
 resource "google_compute_instance" "tf-instance-2" {
   name         = "tf-instance-2"
-  machine_type = "n1-standard-1"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
   allow_stopping_for_update = true
   
   boot_disk {
@@ -30,3 +32,22 @@ resource "google_compute_instance" "tf-instance-2" {
     network = "default"
   }
 }
+
+
+resource "google_compute_instance" "tf-instance-3" {
+  name         = "tf-instance-3"
+  machine_type = "n1-standard-2"
+  zone         = var.zone
+  allow_stopping_for_update = true
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
