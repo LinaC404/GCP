4d04425fbdcba5e54e86bc916ce9f5095c478545
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-21 17:11:45 +0900

    Update storage.tf

--- Automating Infrastructure (Terraform)/modules/storage/storage.tf
+++ Automating Infrastructure (Terraform)/modules/storage/storage.tf
@@ -1,1 +1,10 @@
+provider "google" {
+  project     = "qwiklabs-gcp-03-c5c3b3af1a23"
+  region      = "us-central-1"
+}
 
+resource "google_storage_bucket" "test-bucket-for-state" {
+  name        = "qwiklabs-gcp-03-c5c3b3af1a23"
+  location    = "US"
+  uniform_bucket_level_access = true
+}
