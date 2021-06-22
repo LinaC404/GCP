66f9de2da32062180bf6d5856febd5d05367a947
LinaC_404 <50405887+LinaC404@users.noreply.github.com>
2021-06-22 11:34:20 +0900

    Update variables.tf

--- Automating Infrastructure (Terraform)/modules/storage/variables.tf
+++ Automating Infrastructure (Terraform)/modules/storage/variables.tf
@@ -1,1 +1,14 @@
 
+variable "project_id" {
+    description = "The ID of the project in which to provision resources."
+    type        = string
+    default     = "qwiklabs-gcp-03-7547abdb50f8"
+}
+
+variable "region" {
+    default = "us-central1"
+}
+
+variable "zone" {
+    default = "us-central1-a"
+}
